e6bd43aabcfec026b18f23ff923a5790b9f9e99c
Jashan <jashan.khela@hotmail.com>
2018-06-11 23:42:42 +0000

    step 6 done

--- download_avatar.js
+++ download_avatar.js
@@ -1,9 +1,22 @@
-//Load the request module
+//Load the request module as well as the secrets folder
+var secrets = require('./secrets')
 var request = require('request') ;
 console.log("welcome to the Github Avatar Downloader!");
+console.log(secrets)
 
 //declare a function to take in paramters such as name , repoName and callback function
 function getRepoContributors(repoOwner , repoName , cb) {
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers:{
+      'User-Agent': 'request',
+      'Authorization' : secrets
+
+    }
+  };
+  request(options, function(err , res , body){
+    cb(err, body);
+  })
 }
 
 getRepoContributors ("jquery" , "jquery" , function(err, result){
