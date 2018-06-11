f736f7f765520eaa4e48fc1eedd4451364d050ba
Jashan <jashan.khela@hotmail.com>
2018-06-11 23:08:44 +0000

    This commit has the getRepo function working

--- download_avatar.js
+++ download_avatar.js
@@ -1,2 +1,13 @@
+//Load the request module
 var request = require('request') ;
 console.log("welcome to the Github Avatar Downloader!");
+
+//declare a function to take in paramters such as name , repoName and callback function
+function getRepoContributors(repoOwner , repoName , cb) {
+}
+
+getRepoContributors ("jquery" , "jquery" , function(err, result){
+  console.log("Errors: " , err);
+  console.log("Result: " , result);
+
+})
