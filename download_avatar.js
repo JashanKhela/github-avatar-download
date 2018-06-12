9a38f35cd980e2ae0b2f68ed48bb5d1d48509fd0
Jashan <jashan.khela@hotmail.com>
2018-06-12 01:06:08 +0000

    Downloading all the images

--- download_avatar.js
+++ download_avatar.js
@@ -1,6 +1,7 @@
 //Load the request module as well as the secrets folder
-var secrets = require('./secrets')
+var secrets = require('./secrets');
 var request = require('request') ;
+var fs = require('fs');
 console.log("welcome to the Github Avatar Downloader!");
 console.log(secrets)
 
@@ -11,31 +12,57 @@ function getRepoContributors(repoOwner , repoName , cb) {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers:{
       'User-Agent': 'request',
-      'Authorization' : secrets.GITHUB_TOKEN
 
-    }
+    }, 'auth' :{'bearer': secrets.GITHUB_TOKEN }
   };
   request(options, function(err , res , body){
-     var data = JSON.parse(body)
-
-
+    var data = JSON.parse(body)
     cb(err, data);
   })
 }
 
 getRepoContributors ("jquery" , "jquery" , function(err, result){
-  console.log("Errors: " , err);
-  console.log("Result: " , result);
+  //console.log("Errors: " , err);
+  //console.log("Result: " , result);
+   for(var i = 0; i < result.length; i++){
+  downloadImageByURL(result[i].avatar_url,'./avatars/' + result[i].login + '.jpg')
+
+ }
+
 
 })
 
 function cb(err,data) {
-
   if(err) {
   console.log("We are so sorry, but there seems to be an error")
   return;
   }
  for(var i = 0; i < data.length; i++){
-  console.log(data[0].avatar_url)
+  downloadImageByURL(data[i].avatar_url,'./avatars/' + data[i].login + '.jpg')
+
  }
+
+
 }
+
+function downloadImageByURL(url , filepath){
+
+request.get(url)
+        .on('error', function(err){
+          throw err ;
+        })
+        .on('response', function (response){
+          console.log('Response Status Code: ', response.statusMessage) ;
+          console.log("Content type : " + response.headers['content-type'] )
+
+        })
+        .pipe(fs.createWriteStream(filepath))
+
+
+}
+
+
+
+
+
+
