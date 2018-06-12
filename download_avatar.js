289ace09baa176d9198478bbb9fdb3c4c426db09
Jashan <jashan.khela@hotmail.com>
2018-06-12 00:11:34 +0000

    Step 7

--- download_avatar.js
+++ download_avatar.js
@@ -6,16 +6,20 @@ console.log(secrets)
 
 //declare a function to take in paramters such as name , repoName and callback function
 function getRepoContributors(repoOwner , repoName , cb) {
+
   var options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers:{
       'User-Agent': 'request',
-      'Authorization' : secrets
+      'Authorization' : secrets.GITHUB_TOKEN
 
     }
   };
   request(options, function(err , res , body){
-    cb(err, body);
+     var data = JSON.parse(body)
+    console.log(data)
+
+    cb(err, data);
   })
 }
 
@@ -24,3 +28,17 @@ getRepoContributors ("jquery" , "jquery" , function(err, result){
   console.log("Result: " , result);
 
 })
+
+function cb(err,data) {
+
+  if(err) {
+  console.log("We are so sorry, but there seems to be an error")
+  return;
+  }
+ for(var item in data) {
+  console.log(data[item].avatar_url)
+ }
+
+
+
+ }
